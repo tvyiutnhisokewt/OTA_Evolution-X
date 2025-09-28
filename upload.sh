5641298d69e90de85ab36b90aed39c00026e3dcd
nhansp <nhan.094.vn@gmail.com>
2025-09-29 06:19:12 +0700

    bluejay: 29/09/2025
    Co-authored-by: Yuuki2k6 <phuocduy252@gmail.com>

--- upload.sh
+++ upload.sh
@@ -30,7 +30,7 @@ product_out="$(eval echo ~$USER)/evolution/out/target/product/$1"
 
 # Android and Evolution X version
 android="16.0"
-evo="11.1"
+evo="11.2"
 
 # About us and the device
 maintainer="nhansp ft. yuuki"
